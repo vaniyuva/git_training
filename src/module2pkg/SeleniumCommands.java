20ebd766b99600e3b892fffa08407ff58b61148f
vaniyuva <vaniyuva88@gmail.com>
2019-01-30 19:45:06 -0500

    First commit

---
+++ src/module2pkg/SeleniumCommands.java
@@ -0,0 +1,14 @@
+package module2pkg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumCommands {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	WebDriver driver= new ChromeDriver();
+
+	}
+
+}
